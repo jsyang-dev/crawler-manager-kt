936168b2210f7285be2129c09922affce69fcbb3
Jeongsu Yang <mycat83@gmail.com>
2020-08-21 22:35:57 +0900

    기능: 프로젝트 세팅
    * @EnableConfigServer 애노테이션 추가
    
    해결: #1

--- config-server/src/main/kotlin/link/crawler/config/server/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/link/crawler/config/server/ConfigServerApplication.kt
@@ -2,7 +2,9 @@ package link.crawler.config.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 open class ConfigServerApplication
 
